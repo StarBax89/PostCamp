489552812dad6523ba2a45879b6748cbd51f4bad
Unknown <marcel.bax@studium.fernuni-hagen.de>
2018-02-05 20:31:27 +0100

    remove server

--- api/controllers/maincontroller.js
+++
@@ -1,6 +0,0 @@
-exports.sayHello = function(req, res) {
-
-    var helloWorld = {"Hello": "world"}
-        res.json(helloWorld);
-
-};
--- api/routes/mainrouts.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = function(app) {
-    var mainController = require('../controllers/maincontroller');
-
-    // todoList Routes
-    app.route('/')
-        .get(mainController.sayHello);
-
-};
